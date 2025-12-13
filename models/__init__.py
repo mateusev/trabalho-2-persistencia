d3ceda77ceaeb79bc5c0fc64442ad6c2ea0eaa39
mateusev <mateusevangelista47@gmail.com>
2025-12-12 23:06:47 -0300

    modificação do arquivo env.py para realizar a primeira migração

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+from .aluno import Aluno
+from .carteira_estudantil import Carteira_estudantil
+from .departamento import Departamento
+from .disciplina import Disciplina
+from .matricula import Matricula
+from .professor import Professor
